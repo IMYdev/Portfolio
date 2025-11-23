b53bd553811b4253602fb8615872c91cd58216f3
IMYdev <omranmansour096@gmail.com>
2025-11-23 15:38:24 +0200

    Make the audio muted by default.

--- script.js
+++ script.js
@@ -48,6 +48,8 @@ function triggerFlash(callback) {
 
 unmuteBtn = document.querySelector('.unmute');
 let isMuted = true;
+sfxForward.muted = true;
+sfxBack.muted = true;
 
 unmuteBtn.addEventListener('click', () => {
     isMuted = !isMuted;
